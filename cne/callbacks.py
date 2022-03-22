0dbf882f5d5d8117a3a1b8378def0c728b5af58b
sdamrich <sebastian.damrich@gmx.de>
2022-03-22 11:37:39 +0100

    Add loss computation to logger

--- cne/callbacks.py
+++ cne/callbacks.py
@@ -2,7 +2,7 @@ from openTSNE.callbacks import Callback
 import torch
 import numpy as np
 try:
-    from vis_utils.utils import compute_normalization
+    from vis_utils.utils import compute_normalization, expected_loss_keops, NCE_loss_keops
     vis_utils_available = True
 except:
     vis_utils_available = False
@@ -12,32 +12,72 @@ class Logger(Callback):
                  log_embds=False,
                  log_losses=False,
                  loss_type=None,
+                 graph=None,
                  log_norms=False):
 
         self.log_embds = log_embds
         if self.log_embds:
             self.embds = []
 
-        # to log losses we would need the graph, which we do not have atm...
-        #self.log_losses = log_losses
-        #self.loss_type = loss_type
-        #if self.log_losses:
-        #    assert vis_utils, f"Need vis_utils package to log losses."
-        #    self.losses = []
+        self.log_losses = log_losses
+        self.loss_type = loss_type
+        self.graph = graph.tocoo() if graph is not None else graph
+        if self.log_losses:
+            assert vis_utils_available, f"Need vis_utils package to log losses."
+            self.losses = []
 
         self.log_norm = log_norms
         if self.log_norm:
             assert vis_utils_available, "Need vis_utils package to log normalization constant."
             self.norms = []
 
-    def __call__(self, epoch, model):
-        if self.log_embds:
+    def __call__(self, epoch, model, negative_samples, loss_mode, log_Z=None, noise_in_estimator=None):
+        if self.log_embds or self.log_norm or self.log_losses:
             assert isinstance(model, torch.nn.modules.sparse.Embedding), \
-            f"To log embeddings model must be of type torch.nn.modules.sparse.Embedding but is of type {type(model)}"
-            self.embds.append(model.weight.detach().cpu().numpy())
+            f"To log the model must be of type torch.nn.modules.sparse.Embedding but is of type {type(model)}"
+            embd = model.weight.detach().cpu().numpy()
+
+        if self.log_embds:
+            self.embds.append(embd)
+
+        if self.log_losses:
+            if loss_mode == "UMAP":
+                self.losses.append(expected_loss_keops(high_sim=self.graph,
+                                                       embedding=embd,
+                                                       a=1.0,
+                                                       b=1.0,
+                                                       negative_sample_rate=negative_samples,
+                                                       push_tail=True))
+            elif loss_mode == "ncvis":
+                # transform Z since the implemented criterion does not consider the noise distribution explicitly
+                Z_prime = np.exp(log_Z.detach().cpu().numpy())
+                Z_prime = Z_prime * len(embd)**2
+                self.losses.append(NCE_loss_keops(high_sim=self.graph,
+                                                  embedding=embd,
+                                                  m=negative_samples,
+                                                  Z=Z_prime,
+                                                  a=1.0,
+                                                  b=1.0,
+                                                  noise_log_arg=True,
+                                                  eps=1e-4))
+            elif loss_mode == "neg_sample":
+                # turn noise_in_ratio back into Z via
+                # Z * m * p_n = 1 <--> Z = 1 / (m * p_n)
+                Z = (negative_samples / len(embd)**2)**-1
+                self.losses.append(NCE_loss_keops(high_sim=self.graph,
+                                                  embedding=embd,
+                                                  m=negative_samples,
+                                                  Z=Z,
+                                                  a=1.0,
+                                                  b=1.0,
+                                                  noise_log_arg=True,
+                                                  eps=1e-4))
+            else:
+                raise NotImplementedError("No expected loss function is implemented for InfoNCE")
+
 
         if self.log_norm:
-            self.norms.append(compute_normalization(model.weight.detach().cpu().numpy(),
+            self.norms.append(compute_normalization(embd,
                                                     sim_func="cauchy",
                                                     no_diag=True,
                                                     a=1.0,
