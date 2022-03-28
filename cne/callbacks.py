3bc342b521e8fd5a277fd049f309e6b4dd0a6cb6
sdamrich <sebastian.damrich@gmx.de>
2022-03-28 17:12:16 +0200

    Add KL diveregence logging

--- cne/callbacks.py
+++ cne/callbacks.py
@@ -2,7 +2,8 @@ from openTSNE.callbacks import Callback
 import torch
 import numpy as np
 try:
-    from vis_utils.utils import compute_normalization, expected_loss_keops, NCE_loss_keops
+    from vis_utils.utils import compute_normalization, expected_loss_keops, \
+        NCE_loss_keops, KL_divergence
     vis_utils_available = True
 except:
     vis_utils_available = False
@@ -13,18 +14,26 @@ class Logger(Callback):
                  log_losses=False,
                  loss_type=None,
                  graph=None,
-                 log_norms=False):
+                 log_norms=False,
+                 log_kl=False):
 
         self.log_embds = log_embds
         if self.log_embds:
             self.embds = []
 
+        self.graph = graph.tocoo() if graph is not None else graph
+
         self.log_losses = log_losses
         self.loss_type = loss_type
-        self.graph = graph.tocoo() if graph is not None else graph
         if self.log_losses:
             assert vis_utils_available, f"Need vis_utils package to log losses."
             self.losses = []
+
+        self.log_kl = log_kl
+        if self.log_kl:
+            assert vis_utils_available
+            assert self.graph is not None
+            self.kls = []
 
         self.log_norm = log_norms
         if self.log_norm:
@@ -32,7 +41,7 @@ class Logger(Callback):
             self.norms = []
 
     def __call__(self, epoch, model, negative_samples, loss_mode, log_Z=None, noise_in_estimator=None):
-        if self.log_embds or self.log_norm or self.log_losses:
+        if self.log_embds or self.log_norm or self.log_losses or self.log_kl:
             assert isinstance(model, torch.nn.modules.sparse.Embedding), \
             f"To log the model must be of type torch.nn.modules.sparse.Embedding but is of type {type(model)}"
             embd = model.weight.detach().cpu().numpy()
@@ -75,7 +84,15 @@ class Logger(Callback):
             else:
                 raise NotImplementedError("No expected loss function is implemented for InfoNCE")
 
-
+        if self.log_kl:
+            self.kls.append(KL_divergence(self.graph,
+                                          embedding=embd,
+                                          a=1.0,
+                                          b=1.0,
+                                          sim_func="cauchy",
+                                          eps = float(np.finfo(float).eps),
+                                          norm_over_pos=False)
+                            )
         if self.log_norm:
             self.norms.append(compute_normalization(embd,
                                                     sim_func="cauchy",
